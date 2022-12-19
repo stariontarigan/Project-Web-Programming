df3dbdf0112d2312c23ab43be9d42a71bd7396d6
starion <stariontarigan1030@gmail.com>
2022-12-20 05:21:08 +0700

    project web programming

---
+++ javascript/for.js
@@ -0,0 +1,31 @@
+for (let i = 0; i < 10; i++){
+    console.log(i)
+}
+
+let j = 0
+while (j < 10 ){
+    console.log(j);
+    j++
+}
+
+// array 
+
+
+// tanpa array
+let nama_siswa = "Arni"
+let umur_siswa = 20
+let alamat_siswa ="Bekasi"
+let email_siswa = "arni@gmail.com"
+
+// dengan array
+let data_siswa = ["Arni", 20 , "Bekasi", "arni@gmail.com"];
+
+// nomor index
+console.log(data_siswa[0])
+console.log(data_siswa[1])
+console.log(data_siswa[2])
+console.log(data_siswa[3])
+
+
+// dengan for
+
